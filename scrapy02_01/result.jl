75d1f14bc7fa5a480aba68600750757cc8958e18
marinehayong <marinehayong@gmail.com>
2019-11-20 14:57:47 +0900

    스크래피 실습 조금 더 함 재밌네 세부 문법같은 거는 어려워

---
+++ scrapy02_01/result.jl
@@ -0,0 +1,10 @@
+{"title": "How to use Crawlera with Scrapy"}
+{"title": "Scrapy, Matplotlib and MySQL: Real Estate Data Analysis"}
+{"title": "Web Scraping Questions & Answers Part II"}
+{"title": "Scrapy & AutoExtract API integration"}
+{"title": "Web Scraping Questions & Answers Part I"}
+{"title": "Price intelligence with Python: Scrapy, SQL and Pandas"}
+{"title": "The Web Data Extraction Summit 2019"}
+{"title": "News Data Extraction at Scale with AI powered AutoExtract"}
+{"title": "Gain a Competitive Edge with Product Data"}
+{"title": "Four Use Cases for Online Public Sentiment Data"}
--- scrapy02_01/scrapy02_01/spiders/class02_1.py
+++ scrapy02_01/scrapy02_01/spiders/class02_1.py
@@ -17,8 +17,19 @@ class TestSpider(scrapy.Spider):
         # getall() <-> get(), extract <-> extract_first()
 
         # 예제1(CSS Selector로 해보기)
+
+        # 출력 옵션
+        # scrapy crawl test2 -o result.jl -t jsonlines
+        # -o: 파일명,확장, -t: 파일 타입(json, jsonlines, jl, csv, sml, marshal, pickle)
         for text in response.css('div.post-header h2 a::text').getall():
             # return Type : Request, BaseItem, Dictionary, None
             yield {
                 'title': text
             }
+
+        # 예제2(XPath)
+        # for i, text in enumerate(response.xpath('//div[@class="post-header"]/h2/a/text()').getall()):
+        #     yield {
+        #         'number': i,
+        #         'title': text
+        #     }
